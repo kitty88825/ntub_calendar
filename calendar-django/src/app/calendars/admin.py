fd1d6dfe9b8a0f9cb21a3d5e0baf3fdd09f694cb
kitty88825 <kitty40632@gmail.com>
2020-05-08 14:30:08 +0800

    增加calendars資料表欄位

--- calendar-django/src/app/calendars/admin.py
+++ calendar-django/src/app/calendars/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Calendar, Subscription, Permission
+
+
+@admin.register(Calendar)
+class CalendarAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'calendar_id')
+
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ('group_id', 'calendar_id', 'role')
--- calendar-django/src/app/calendars/models.py
+++ calendar-django/src/app/calendars/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import Group
 
-# Create your models here.
+from app.users.models import User
+
+
+class Calendar(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+
+class Subscription(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    calendar_id = models.ForeignKey(Calendar, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['user_id', 'calendar_id']
+
+
+class Permission(models.Model):
+    role_choice = [
+        ('r', '可看'),
+        ('w', '可讀'),
+    ]
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    calendar_id = models.ForeignKey(Calendar, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=role_choice)
+
+    class Meta:
+        unique_together = ['group_id', 'calendar_id']
