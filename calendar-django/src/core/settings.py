e9f08fd6d077efd72cbbee977cb2640b0fdeaef2
kitty88825 <kitty40632@gmail.com>
2020-05-08 15:42:30 +0800

    安裝djangorestframework-camel-case解決駝峰命名問題

--- calendar-django/src/core/settings.py
+++ calendar-django/src/core/settings.py
@@ -155,3 +155,22 @@ NTUB_AUTH_API_DOMAIN = env('NTUB_AUTH_API_DOMAIN')
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:4200',
 ]
+
+# REST FRAMEWORK
+REST_FRAMEWORK = {
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    ),
+
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
+
+    'JSON_UNDERSCOREIZE': {
+        'no_underscore_before_number': True,
+    },
+}
