7dd8396e588e8f6a5e5ad468916bbb6b0c8da5e1
kitty88825 <kitty40632@gmail.com>
2020-05-08 14:30:17 +0800

    增加events資料表欄位

--- calendar-django/src/app/events/admin.py
+++ calendar-django/src/app/events/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Attachment, Event, Participant
+
+
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    pass
--- calendar-django/src/app/events/models.py
+++ calendar-django/src/app/events/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from app.calendars.models import Calendar
+from app.users.models import User
+
+
+class Attachment(models.Model):
+    name = models.FileField(upload_to='uploads/%Y%m%d/%H%M%S/')
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    start_at = models.DateTimeField()
+    end_at = models.DateTimeField()
+    end_at = models.DateTimeField()
+    description = models.TextField(null=True, blank=True)
+    create_at = models.DateTimeField()
+    update_at = models.DateTimeField()
+    location = models.TextField(null=True, blank=True)
+    attachments = models.ManyToManyField(Attachment,
+                                         blank=True)
+    calendars = models.ManyToManyField(Calendar)
+
+
+class Participant(models.Model):
+    role_choice = [
+        ('editors', '編輯者'),
+        ('participants', '參與者'),
+    ]
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    role = models.CharField(max_length=15, choices=role_choice)
+
+    class Meta:
+        unique_together = ['user_id', 'event_id']
