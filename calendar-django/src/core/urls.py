3868dc46fe2111fa3994860c4184fd35086fd66c
kitty88825 <kitty40632@gmail.com>
2020-05-08 15:33:10 +0800

    安裝drf_yasg套件＆查看sagger

--- calendar-django/src/core/urls.py
+++ calendar-django/src/core/urls.py
@@ -17,13 +17,28 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from app.users.views import AccountView
 
 router = DefaultRouter(False)
-router.register('account', AccountView, 'sign_account')
+router.register('', AccountView, 'sign_account')
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Book Store API",
+      default_version='v1',
+      description="My Book Store",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
