3013a929b3b923208a8d4718a75f5ce3bfc649ae
kitty88825 <kitty40632@gmail.com>
2020-05-13 20:48:30 +0800

    Revert "建立Attachment的CRUD"
    This reverts commit dd3ee7ba6830e3f96bffd5c96615b8a51f3b75b0.

--- calendar-django/src/app/events/serializers.py
+++ calendar-django/src/app/events/serializers.py
@@ -1,15 +1,9 @@
 from rest_framework import serializers
 
-from .models import Event, Attachment
+from .models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         exclude = ['create_at', 'update_at']
-
-
-class AttachmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Attachment
-        fields = '__all__'
--- calendar-django/src/app/events/views.py
+++ calendar-django/src/app/events/views.py
@@ -1,14 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Event, Attachment
-from .serializers import EventSerializer, AttachmentSerializer
+from .models import Event
+from .serializers import EventSerializer
 
 
 class EventViewSet(ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-
-
-class AttachmentViewSet(ModelViewSet):
-    queryset = Attachment.objects.all()
-    serializer_class = AttachmentSerializer
