60a7dd1378a7393522bfb1cbd951b61f4ed34988
kitty88825 <kitty40632@gmail.com>
2020-05-14 23:05:50 +0800

    移除deploy檔案

--- calendar-django/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cp -rf Pipfile docker/
-cp -rf Pipfile.lock docker/
-docker-compose build
-docker-compose down
-docker-compose up -d
-rm -rf docker/Pipfile
-rm -rf docker/Pipfile.lock
--- calendar-django/docker/runcelery.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-celery -A config worker -l info -B
-exec "$@"
