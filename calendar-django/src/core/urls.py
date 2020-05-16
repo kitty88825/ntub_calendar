b71627240725c5a1f72300b9e091bb414444906d
kitty88825 <kitty40632@gmail.com>
2020-05-16 17:14:32 +0800

    顯示attachment檔案

--- calendar-django/src/core/urls.py
+++ calendar-django/src/core/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import permissions
 
@@ -42,3 +44,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
