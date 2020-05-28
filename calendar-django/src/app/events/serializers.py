c02cde68fec80a222aac2c8800bf2972c1d709b0
kitty88825 <kitty40632@gmail.com>
2020-05-28 20:45:17 +0800

    將參與者建立到Participant資料表中

--- calendar-django/src/app/events/serializers.py
+++ calendar-django/src/app/events/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from app.users.models import User
+
 from .models import Attachment, Event, Participant
 
 
@@ -14,6 +16,12 @@ class AttachmentSerializer(serializers.ModelSerializer):
         return str(attachment.file).split('/')[-1]
 
 
+class ParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Participant
+        fields = ('id', 'user')
+
+
 class EventSerializer(serializers.ModelSerializer):
     files = serializers.ListField(
         child=serializers.FileField(),
@@ -22,6 +30,12 @@ class EventSerializer(serializers.ModelSerializer):
     )
     attachments = AttachmentSerializer(many=True, read_only=True)
     user = serializers.CharField(write_only=True, required=False)
+    participants = serializers.ListField(
+        child=serializers.EmailField(),
+        write_only=True,
+        required=False,
+    )
+    participant_set = ParticipantSerializer(many=True, read_only=True)
 
     class Meta:
         model = Event
@@ -36,12 +50,15 @@ class EventSerializer(serializers.ModelSerializer):
             'attachments',
             'calendars',
             'user',
+            'participants',
+            'participant_set',
         )
         read_only_fields = (
             'id',
             'create_at',
             'update_at',
             'attachments',
+            'participant_set',
         )
 
     def create_attachment_for_event(self, event, files):
@@ -52,16 +69,32 @@ class EventSerializer(serializers.ModelSerializer):
             [Attachment(event=event, file=file) for file in files],
         )
 
-    def create(self, validated_data):
-        user = validated_data.pop('user', None)
-        files = validated_data.pop('files', None)
-        event = super().create(validated_data)
+    def create_participant_for_event(self, event, user, participants):
         Participant.objects.create(
             event=event,
             user=user,
             role=Participant.RoleChoice.editors,
         )
+
+        if not participants:
+            return
+
+        users = User.objects.filter(email__in=participants)
+        Participant.objects.bulk_create(
+            [Participant(
+                event=event,
+                user=user,
+                role=Participant.RoleChoice.participants,
+            ) for user in users],
+        )
+
+    def create(self, validated_data):
+        user = validated_data.pop('user')
+        files = validated_data.pop('files', None)
+        participants = validated_data.pop('participants', None)
+        event = super().create(validated_data)
         self.create_attachment_for_event(event, files)
+        self.create_participant_for_event(event, user, participants)
         return event
 
 
