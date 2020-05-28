3c20618c3f290d27eee28ba15babac792ee5d284
jafn <10646024@ntub.edu.tw>
2020-05-28 14:08:41 +0800

    修改事件敘述

--- calendar_angular/src/app/app.module.ts
+++ calendar_angular/src/app/app.module.ts
@@ -25,8 +25,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpBodyInterceptor } from './interceptors/http-body.interceptor';
 import { EditScheduleComponent } from './edit-schedule/edit-schedule.component';
 import { FilterPipe } from '../app/filter.pipe'; // -> imported filter pipe
-import { AppHttpInterceptorService } from './services/app-http-interceptor.service';
-
 
 @NgModule({
   declarations: [
