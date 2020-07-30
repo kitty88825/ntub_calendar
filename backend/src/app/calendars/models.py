ce062723023f4ec00eb0819f6b10af5b27c64fe0
kitty88825 <kitty40632@gmail.com>
2020-07-30 11:53:21 +0800

    新增 display 不能為 null 和 blank

--- backend/src/app/calendars/models.py
+++ backend/src/app/calendars/models.py
@@ -11,7 +11,12 @@ class Calendar(models.Model):
     ]
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True, null=True)
-    display = models.CharField(max_length=10, choices=display_choice, default='private')  # noqa: E501
+    display = models.CharField(
+        max_length=10,
+        choices=display_choice,
+        blank=False,
+        null=False,
+    )
 
     def __str__(self):
         return self.name
