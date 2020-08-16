06bf809d85afbd4f8862f706d60f2d918f4ecbd5
linda314014 <l940026l@gmail.com>
2020-08-16 17:45:38 +0800

    根據calendar的display顯示不同的event

--- backend/src/app/events/views.py
+++ backend/src/app/events/views.py
@@ -1,13 +1,32 @@
-from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
+
 from rest_framework.viewsets import ModelViewSet
+
+from app.calendars.models import Permission, Calendar
+from app.calendars.permission import IsStaffUserEditOnly
+from app.users.models import User
 
 from .models import Event
 from .serializers import EventSerializer, UpdateAttachmentSerializer
 
 
 class EventViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsStaffUserEditOnly]
     queryset = Event.objects.prefetch_related('attachments')
+
+    def get_queryset(self):
+        calendar_public = Calendar.objects.filter(display='public')
+        if self.request.user.is_authenticated:
+            user_group = User.objects.filter(id=self.request.user.id).values('groups')  # noqa 501
+            calendar_id = Permission.objects.values('calendar').filter(group__in=user_group)  # noqa 501
+            return Event.objects \
+                .filter(
+                    Q(calendars__in=calendar_id) |
+                    Q(calendars__in=calendar_public),
+                ) \
+                .distinct()
+        else:
+            return Event.objects.filter(calendars__in=calendar_public)
 
     def get_serializer_class(self):
         if self.action == 'partial_update':
