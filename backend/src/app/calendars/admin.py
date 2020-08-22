95183f91fbc66ca1ee46dc993a2df4115af891bf
kitty88825 <kitty40632@gmail.com>
2020-08-22 20:56:05 +0800

    CalendarAdmin filter_vertical 增加 subscribers

--- backend/src/app/calendars/admin.py
+++ backend/src/app/calendars/admin.py
@@ -6,6 +6,7 @@ from .models import Calendar, CalendarPermission
 @admin.register(Calendar)
 class CalendarAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description', 'display')
+    filter_vertical = admin.ModelAdmin.filter_vertical + ('subscribers',)
 
 
 @admin.register(CalendarPermission)
