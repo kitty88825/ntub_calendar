6a5a887e1aab125577e0d3158b060f287fe5520d
jafn <10646024@ntub.edu.tw>
2020-08-30 17:04:20 +0800

    完成新增行事曆畫面

--- frontend/src/app/add-calendar/add-calendar.component.ts
+++ frontend/src/app/add-calendar/add-calendar.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-calendar.component.scss']
 })
 export class AddCalendarComponent implements OnInit {
+  isCollapsed = false;
+  isMeet = true;
+  isSchedule = !this.isMeet;
+  attribute = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  meet(value) {
+
+    if (value.target.checked === true) {
+      this.isSchedule = false;
+      this.attribute = value.target.value;
+    } else {
+      this.isSchedule = true;
+      this.attribute = '不公開';
+    }
+    console.log(this.attribute);
+  }
+
+  schedule(value) {
+
+    if (value.target.checked === true) {
+      this.isMeet = false;
+      this.attribute = value.target.value;
+    } else {
+      this.isMeet = true;
+      this.attribute = '公開';
+    }
+    console.log(this.attribute);
+  }
+
+  addCalendar() {
+
   }
 
 }
