7865d1f8e02e9f6aa51f85ab5384f8c1d6ede323
kitty88825 <kitty40632@gmail.com>
2020-09-09 16:16:20 +0800

    Use email as username

--- backend/src/app/users/models.py
+++ backend/src/app/users/models.py
@@ -1,13 +1,46 @@
 import uuid
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from .choices import RoleChoice
 
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email, and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(_('email address'), unique=True)
     code = models.UUIDField(default=uuid.uuid4, editable=True)
     role = models.CharField(
         max_length=15,
@@ -15,8 +48,17 @@ class User(AbstractUser):
         default=RoleChoice.unknow,
     )
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
     def __str__(self):
         return self.email
+
+    @property
+    def username(self):
+        return self.email.split('@')[0]
 
 
 class CommonParticipant(models.Model):
