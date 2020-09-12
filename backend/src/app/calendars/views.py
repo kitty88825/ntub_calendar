e6ef2bb9917c51e262db0004497f39f7e92a4f82
kitty88825 <kitty40632@gmail.com>
2020-09-12 16:11:56 +0800

    Calendar and CalendarPermissions CRUD api finish

--- backend/src/app/calendars/views.py
+++ backend/src/app/calendars/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.db.models import Q
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Calendar
+from .serializers import CalendarSerializer
+from .permission import IsStaffUserEditOnly
+
+
+class CalendarViewSet(ModelViewSet):
+    queryset = Calendar.objects.all()
+    serializer_class = CalendarSerializer
+    permission_classes = [IsStaffUserEditOnly]
+
+    def get_queryset(self):
+        if self.request.user.id:
+            return Calendar.objects \
+                .filter(
+                    Q(permissions__group__user=self.request.user.id) |
+                    Q(display='public'),
+                ) \
+                .distinct()
+        else:
+            return Calendar.objects.filter(display='public')
