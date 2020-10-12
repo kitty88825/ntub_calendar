110dee75a99f98a8e955eda53eaab364850210ec
kitty88825 <kitty40632@gmail.com>
2020-10-13 01:06:24 +0800

    Add api calendar/calendars?subscribed=boolean

---
+++ backend/src/app/calendars/filters.py
@@ -0,0 +1,21 @@
+from django_filters import rest_framework as filters
+
+from .models import Calendar
+
+
+class SubscriberCalendarsFilter(filters.FilterSet):
+
+    subscribed = filters.BooleanFilter(
+        field_name='subscribers',
+        method='filter_subscribed',
+    )
+
+    class Meta:
+        model = Calendar
+        fields = ['subscribed']
+
+    def filter_subscribed(self, queryset, name, value):
+        if not value or not self.request.user.is_authenticated:
+            return queryset
+
+        return queryset.filter(**{name: self.request.user.id})
--- backend/src/app/calendars/views.py
+++ backend/src/app/calendars/views.py
@@ -2,18 +2,23 @@ from django.db.models import Q
 
 from rest_framework.viewsets import ModelViewSet
 
+from django_filters import rest_framework as filters
+
 from .models import Calendar
 from .serializers import CalendarSerializer
 from .permission import IsStaffUserEditOnly
+from .filters import SubscriberCalendarsFilter
 
 
 class CalendarViewSet(ModelViewSet):
     queryset = Calendar.objects.all()
     serializer_class = CalendarSerializer
     permission_classes = [IsStaffUserEditOnly]
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = SubscriberCalendarsFilter
 
     def get_queryset(self):
-        if self.request.user.id:
+        if self.request.user.is_authenticated:
             return Calendar.objects \
                 .filter(
                     Q(permissions__group__user=self.request.user.id) |
