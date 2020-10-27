bbeef1582c0c08ca88c5c96174755a0002d4e6c1
kitty88825 <kitty40632@gmail.com>
2020-10-27 15:56:16 +0800

    Add event invite calendar to the EventAdmin

--- backend/src/app/events/admin.py
+++ backend/src/app/events/admin.py
@@ -1,6 +1,39 @@
 from django.contrib import admin
 
-from .models import Event, EventAttachment, EventParticipant
+from .models import (
+    Event,
+    EventAttachment,
+    EventParticipant,
+    EventInviteCalendar,
+)
+
+
+@admin.register(EventInviteCalendar)
+class EventInviteCalendarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'event', 'calendar', 'main_calendar', 'response')
+
+
+class EventInviteCalendarInlineAdmin(admin.TabularInline):
+    model = EventInviteCalendar
+    extra = 1
+
+
+@admin.register(EventParticipant)
+class EventParticipantAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user_id',
+        'user',
+        'event_id',
+        'event',
+        'role',
+        'response',
+    )
+
+
+class EventParticipantInlineAdmin(admin.TabularInline):
+    model = EventParticipant
+    extra = 1
 
 
 @admin.register(EventAttachment)
@@ -15,7 +48,11 @@ class EventAttachmentInlineAdmin(admin.TabularInline):
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    inlines = (EventAttachmentInlineAdmin,)
+    inlines = (
+        EventInviteCalendarInlineAdmin,
+        EventParticipantInlineAdmin,
+        EventAttachmentInlineAdmin,
+    )
     list_display = (
         'id',
         'title',
@@ -25,18 +62,4 @@ class EventAdmin(admin.ModelAdmin):
         'nature',
         'location',
     )
-    filter_vertical = admin.ModelAdmin.filter_vertical + \
-        ('calendars', 'subscribers')
-
-
-@admin.register(EventParticipant)
-class EventParticipantAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'user_id',
-        'user',
-        'event_id',
-        'event',
-        'role',
-        'response',
-    )
+    filter_vertical = admin.ModelAdmin.filter_vertical + ('subscribers',)
