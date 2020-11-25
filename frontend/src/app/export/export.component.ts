140c50deee875228ce3fa5cff8bcea8df707f773
keweicheng <10646014@ntub.edu.tw>
2020-11-25 22:49:59 +0800

    調整下載pdf程式碼、調整scss程式碼

--- frontend/src/app/export/export.component.ts
+++ frontend/src/app/export/export.component.ts
@@ -135,27 +135,26 @@ export class ExportComponent implements OnInit {
       });
     }
     html2canvas(this.screen.nativeElement).then(canvas => {
-      const contentWidth = canvas.width;
-      const contentHeight = canvas.height;
-      console.log(canvas.width);
-      console.log(canvas.height);
+      const pageWidth = 841.89;
+      const pageHeight = 592.28;
+      const contentWidth = canvas.width*3/4;
+      const contentHeight = canvas.height*3/4;
+      const imgWidth = pageWidth;
+      const imgHeight = pageWidth / contentWidth * contentHeight;
 
-      const pageHeight = contentWidth / 592.28 * 841.89;
-      let leftHeight = contentHeight;
-      let position = 0;
+      let position = 5;
+      let leftHeight = imgHeight;
 
-      const imgWidth = 590; //595.28
-      const imgHeight = 592.28 / contentWidth * contentHeight;  //592.28
       const pageData = canvas.toDataURL('image/jpeg', 1.0);
-      const pdf = new jsPDF('1', 'pt', 'a4');
+      const pdf = new jsPDF('landscape', 'pt', 'a4');
 
       if (leftHeight <= pageHeight) {
-        pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight);
+        pdf.addImage(pageData, 'JPEG', 5, 10, imgWidth, imgHeight);
       } else {
         while (leftHeight > 0) {
-          pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+          pdf.addImage(pageData, 'JPEG', 5, position, imgWidth, imgHeight)
           leftHeight -= pageHeight;
-          position -= 881;
+          position -= pageHeight;
           if (leftHeight > 0) {
             pdf.addPage();
           }
