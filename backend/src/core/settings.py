b1057b8d0a552edbd4e34e9becb94c2846ba2e54
kitty88825 <kitty40632@gmail.com>
2020-11-29 21:39:07 +0800

    Add url to the settings and .env

--- backend/src/core/settings.py
+++ backend/src/core/settings.py
@@ -181,3 +181,4 @@ EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
+URL = env('URL')
