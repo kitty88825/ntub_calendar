3e6186d692be5cee2633d8d4409c8017311a2baf
kitty88825 <kitty40632@gmail.com>
2020-12-01 14:04:25 +0800

    Add time field of the event get filter

--- backend/src/app/events/filters.py
+++ backend/src/app/events/filters.py
@@ -1,3 +1,5 @@
+import calendar
+
 from django_filters import rest_framework as filters
 
 from .models import Event
@@ -8,13 +10,24 @@ class SubscriberEventsFilter(filters.FilterSet):
         field_name='subscribers',
         method='filter_subscribed',
     )
+    time = filters.DateTimeFilter(
+        field_name='start_at',
+        method='filter_time',
+    )
 
     class Meta:
         model = Event
-        fields = ['subscribed', 'nature']
+        fields = ['subscribed', 'nature', 'time']
 
     def filter_subscribed(self, queryset, name, value):
         if not value or not self.request.user.is_authenticated:
             return queryset
 
         return queryset.filter(**{name: self.request.user.id})
+
+    def filter_time(self, queryset, name, value):
+        start = value
+        month_end = calendar.monthrange(value.year, start.month)[1]
+        end = value.replace(day=month_end)
+
+        return queryset.filter(start_at__range=[start, end])
