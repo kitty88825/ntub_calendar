26396c92724d6bec4cb9c32cfb6a87dc13de6cb3
kitty88825 <kitty40632@gmail.com>
2020-12-10 20:56:34 +0800

    Remove --noinput of the entrypoint.sh

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -10,16 +10,16 @@ echo "Migrating Database"
 python manage.py migrate --noinput
 
 echo "Loaddata group.json"
-python manage.py loaddata group.json  --noinput
+python manage.py loaddata group.json
 
 echo "Loaddata calendar.json"
-python manage.py loaddata calendar.json  --noinput
+python manage.py loaddata calendar.json
 
 echo "Loaddata super.json"
-python manage.py loaddata super.json  --noinput
+python manage.py loaddata super.json
 
 echo "Loaddata permission.json"
-python manage.py loaddata permission.json  --noinput
+python manage.py loaddata permission.json
 
 echo "Run django Q"
 python manage.py qcluster --traceback
