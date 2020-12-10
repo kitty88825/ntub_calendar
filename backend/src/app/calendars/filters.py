bb71985bb4c76a3cec9042efa75efefa080f9651
kitty88825 <kitty40632@gmail.com>
2020-12-10 16:40:38 +0800

    Add calendar permission filter to the Calendar View

--- backend/src/app/calendars/filters.py
+++ backend/src/app/calendars/filters.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from django_filters import rest_framework as filters
 
 from .models import Calendar
@@ -9,6 +11,11 @@ class SubscriberCalendarsFilter(filters.FilterSet):
         method='filter_subscribed',
     )
 
+    permission = filters.BooleanFilter(
+        field_name='groups',
+        method='filter_permission',
+    )
+
     class Meta:
         model = Calendar
         fields = ['subscribed']
@@ -18,3 +25,13 @@ class SubscriberCalendarsFilter(filters.FilterSet):
             return queryset
 
         return queryset.filter(**{name: self.request.user.id})
+
+    def filter_permission(self, queryset, name, value):
+        if not value or not self.request.user.is_authenticated:
+            return queryset.filter(name='')
+
+        return queryset \
+            .filter(
+                Q(permissions__role=self.request.user.role),
+                Q(permissions__group__user=self.request.user)) \
+            .distinct()
